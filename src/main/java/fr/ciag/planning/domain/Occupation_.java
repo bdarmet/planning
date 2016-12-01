776f0fd984dda154be1d987655694b1ee0f6db25
bdarmet <b_darmet@ciag.fr>
2016-12-01 10:34:41 +0100

    resolution des erreurs du à la configuration des projets

--- src/main/java/fr/ciag/planning/domain/Occupation_.java
+++ src/main/java/fr/ciag/planning/domain/Occupation_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2016-11-30T18:22:37.637+0100")
+@Generated(value="Dali", date="2016-12-01T10:16:57.157+0100")
 @StaticMetamodel(Occupation.class)
 public class Occupation_ {
 	public static volatile SingularAttribute<Occupation, Integer> id;
@@ -14,6 +14,6 @@ public class Occupation_ {
 	public static volatile SingularAttribute<Occupation, Boolean> validation;
 	public static volatile SingularAttribute<Occupation, Boolean> present;
 	public static volatile SingularAttribute<Occupation, Boolean> prestation;
-	public static volatile SingularAttribute<Occupation, Groupe> groupe;
 	public static volatile SingularAttribute<Occupation, String> couleur;
+	public static volatile SingularAttribute<Occupation, Groupe> groupe;
 }
